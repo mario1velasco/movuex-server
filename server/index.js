f5c79d0233c3f1e69cccbc1b00dd9f78ec700fb2
Andrea Zucchini <azga84@gmail.com>
2017-05-21 23:50:46 +0200

    change folder file

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
 import http from 'http'
 import express from 'express'
-import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import socketio from 'socket.io'
+import cors from 'cors'
+import { realTime } from './realTime'
+import { requestLogger } from './middlewares/requestLogger'
+import { configureMongo } from './Infrastructure/Persistence/mongoose'
 
-import api from './api'
+import api from './api/api'
 
-mongoose.Promise = Promise
-mongoose.connect('mongodb://localhost/movuex')
+configureMongo()
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(requestLogger())
 app.use('/api', api)
 
 let port = process.env.PORT || 3000
 
 io.on('connection', (socket) => {
   console.log(`Connected socket with id: ${socket.id}`)
-  socket.on('ping', () => socket.emit('pong'))
+  realTime(io, socket)
 })
 server.listen(port, () => console.log('server listen to port 3000'))
