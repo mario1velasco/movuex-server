2522964cac686afbf38852512ad23d63cbd86273
Andrea Zucchini <azga84@gmail.com>
2017-05-27 10:39:31 +0200

    db connection string

--- server/Infrastructure/Persistence/Mongo/mongoose.js
+++ server/Infrastructure/Persistence/Mongo/mongoose.js
@@ -3,5 +3,5 @@ import Constants from './Constants'
 export const configureMongo = () => {
   mongoose.Promise = Promise
 // TODO set configuration
-  mongoose.connect(Constants.DB_CONNECTION_STRING)
+  mongoose.connect(`${Constants.DB_CONNECTION_STRING}/${Constants.DB_COLLECTION_NAME}`)
 }
