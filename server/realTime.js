14ef7d3efd62e3ad2856df28cf674ddd8348c502
Andrea Zucchini <azga84@gmail.com>
2017-05-28 21:38:57 +0200

    remove unnecessary files

--- server/realTime.js
+++
@@ -1,7 +0,0 @@
-export const realTime = (io, socket) => {
-  socket.on('ping', () => socket.emit('pong'))
-  socket.on('addVote', (showId) => {
-    console.log(showId, ' se ha votado')
-    io.sockets.emit('vote:added', 'successful ' + showId)
-  })
-}
