a9240d02df1d04fa51c186a73a04ae3a34b31482
Andrea Zucchini <azga84@gmail.com>
2017-05-28 21:32:10 +0200

    mock data

---
+++ test/mockData/mockData.js
@@ -0,0 +1,100 @@
+export const mockData = [
+  {
+    '_id': '59289c72c512526c5715c04b',
+    'id': 1,
+    'url': 'http://www.tvmaze.com/shows/1/under-the-dome',
+    'name': 'Under the Dome',
+    'type': 'Scripted',
+    'language': 'English',
+    'genres': [
+      'Drama',
+      'Science-Fiction',
+      'Thriller'
+    ],
+    'image': {
+      'medium': 'http://static.tvmaze.com/uploads/images/medium_portrait/0/1.jpg',
+      'original': 'http://static.tvmaze.com/uploads/images/original_untouched/0/1.jpg'
+    },
+    'status': 'Ended',
+    'runtime': 60,
+    'premiered': '2013-06-24',
+    'schedule': {
+      'time': '22:00',
+      'days': [
+        'Thursday'
+      ]
+    },
+    'rating': {
+      'average': 6.6
+    },
+    'weight': 0,
+    'network': {
+      'id': 2,
+      'name': 'CBS',
+      'country': {
+        'name': 'United States',
+        'code': 'US',
+        'timezone': 'America/New_York'
+      }
+    },
+    'webChannel': null,
+    'externals': {
+      'tvrage': 25988,
+      'thetvdb': 264492,
+      'imdb': 'tt1553656'
+    },
+    'summary': '<p><strong>Under the Dome</strong> is the story of a small town that is suddenly and inexplicably sealed off from the rest of the world by an enormous transparent dome. The town\'s inhabitants must deal with surviving the post-apocalyptic conditions while searching for answers about the dome, where it came from and if and when it will go away.</p>',
+    'updated': 1490211618,
+    '_links': {
+      'self': {
+        'href': 'http://api.tvmaze.com/shows/1'
+      },
+      'previousepisode': {
+        'href': 'http://api.tvmaze.com/episodes/185054'
+      }
+    }
+  },
+  {
+    '_id': '59289c72c512526c5715c04c',
+    'id': 2,
+    'url': 'http://www.tvmaze.com/shows/2/person-of-interest',
+    'name': 'Person of Interest',
+    'type': 'Scripted',
+    'language': 'English',
+    'genres': [
+      'Drama',
+      'Action',
+      'Crime',
+      'Science-Fiction',
+      'Thriller',
+      'Mystery'
+    ],
+    'status': 'Ended',
+    'runtime': 60,
+    'premiered': '2011-09-22',
+    'schedule': {
+      'time': '22:00',
+      'days': [
+        'Tuesday'
+      ]
+    },
+    'rating': {},
+    'weight': 95,
+    'webChannel': null,
+    'externals': {
+      'tvrage': 28376,
+      'thetvdb': 248742,
+      'imdb': 'tt1839578'
+    },
+    'summary': '<p>You are being watched. The government has a secret system, a machine that spies on you every hour of every day. I know because I built it. I designed the Machine to detect acts of terror but it sees everything. Violent crimes involving ordinary people. People like you. Crimes the government considered "irrelevant." They wouldn\'t act so I decided I would. But I needed a partner. Someone with the skills to intervene. Hunted by the authorities, we work in secret. You\'ll never find us. But victim or perpetrator, if your number is up, we\'ll find you.</p>',
+    'updated': 1492964836,
+    '_links': {
+      'self': {
+        'href': 'http://api.tvmaze.com/shows/2'
+      },
+      'previousepisode': {
+        'href': 'http://api.tvmaze.com/episodes/659372'
+      }
+    }
+  }
+]
