2e7a056afaa5124bb13cfbcb771c30ef3831c7fa
Andrea Zucchini <azga84@gmail.com>
2017-05-28 23:03:35 +0200

    auth component

---
+++ server/config/components/auth/auth.js
@@ -0,0 +1,7 @@
+export const authConfig = {
+  'twitterAuth': {
+    'consumerKey': 'Ta2KR96inwOjEYhrrScPR0Veq',
+    'consumerSecret': 'gHdcbLoRpD9DxTNZXaAvxYnfvLEHMb1kV2P44jRpQvOkpdjMaf',
+    'callbackURL': 'http://localhost:8080/api/user/twitter/callback'
+  }
+}
