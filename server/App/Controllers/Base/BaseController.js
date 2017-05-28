8d89416da1da226ce41cb1efeb59b9599ed5c82f
Andrea Zucchini <azga84@gmail.com>
2017-05-28 21:30:38 +0200

    change structure

--- server/App/Controllers/Base/BaseController.js
+++
@@ -1,8 +0,0 @@
-import logger from '../../../Infrastructure/Persistence/Logger/Logger'
-
-export class BaseController {
-  checkForError (res, err) {
-    logger.error(err.stack)
-    res.send({error: 'error in your request'})
-  }
-}
--- server/Infrastructure/Persistence/Mongo/mongoose.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import Constants from './Constants'
-export const configureMongo = () => {
-  mongoose.Promise = Promise
-// TODO set configuration
-  mongoose.connect(`${Constants.DB_CONNECTION_STRING}/${Constants.DB_COLLECTION_NAME}`)
-}
