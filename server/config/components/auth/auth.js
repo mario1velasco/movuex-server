c53b19f442268a9af670c4e57658e7673e3b1363
Andrea Zucchini <azga84@gmail.com>
2017-05-28 23:25:44 +0200

    use api key

--- server/config/components/auth/auth.js
+++ server/config/components/auth/auth.js
@@ -1,7 +1,7 @@
 export const authConfig = {
   'twitterAuth': {
-    'consumerKey': 'Ta2KR96inwOjEYhrrScPR0Veq',
-    'consumerSecret': 'gHdcbLoRpD9DxTNZXaAvxYnfvLEHMb1kV2P44jRpQvOkpdjMaf',
+    'consumerKey': 'your-consumer-key-here',
+    'consumerSecret': 'your-client-secret-here',
     'callbackURL': 'http://localhost:8080/api/user/twitter/callback'
   }
 }
