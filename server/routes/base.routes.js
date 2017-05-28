55c75daff4fde75b380092f4e0608e2142d3efbf
Andrea Zucchini <azga84@gmail.com>
2017-05-28 23:04:29 +0200

    use user routes

--- server/routes/base.routes.js
+++ server/routes/base.routes.js
@@ -1,8 +1,11 @@
 import { ShowRoutes } from './show.routes'
+import { UserRoutes } from './user.routes'
 
 export class BaseRoutes {
-  static createRoutes (app) {
+  static createRoutes (app, passport) {
     const showRoutes = new ShowRoutes()
+    const userRoutes = new UserRoutes(passport)
     app.use('/api', showRoutes.createRoutes())
+    app.use('/api/user', userRoutes.createRoutes())
   }
 }
