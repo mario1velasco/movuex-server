b2f5c9f5482ece9c09491deaaa36b9c79777c649
Andrea Zucchini <azga84@gmail.com>
2017-05-28 23:04:07 +0200

    use passport

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import http from 'http'
 import express from 'express'
-import { Socket } from './config/components/socket'
+import { Socket } from './config/components/socket/socket'
 import { BaseMiddlewares } from './config/middlewares/base.middlewares'
 import { connectWithMongoDB } from './config/infrastructure/persistence/mongo/mongoose'
 import { BaseRoutes } from './routes/base.routes'
-
+import passport from 'passport'
+import { Passport } from './config/components/passport/passport'
+import session from 'express-session'
+Passport.configure(passport)
 connectWithMongoDB()
 
 const app = express()
@@ -14,8 +17,16 @@ socket.on('socket-opened', (socket) => {
   console.log(`Connected socket with id: ${socket.id}`)
 })
 
+app.use(session({
+  secret: 'ilovenodejs',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 BaseMiddlewares.config(app)
-BaseRoutes.createRoutes(app)
+BaseRoutes.createRoutes(app, passport)
 
 const port = process.env.PORT || 3000
 server.listen(port, () => console.log(`server listen to port ${port}`))
