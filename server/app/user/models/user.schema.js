4a1fae34fde8dd66880b920bbdc2abdc33b31a93
Andrea Zucchini <azga84@gmail.com>
2017-05-28 23:03:18 +0200

    user model

--- server/app/user/models/user.schema.js
+++ server/app/user/models/user.schema.js
@@ -1,37 +1,9 @@
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt-nodejs'
-const userSchema = mongoose.Schema({
-  local: {
-    email: String,
-    password: String
-  },
-  facebook: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
-  },
+export const userSchema = mongoose.Schema({
   twitter: {
     id: String,
     token: String,
     displayName: String,
     username: String
-  },
-  google: {
-    id: String,
-    token: String,
-    email: String,
-    name: String
   }
 })
-
-userSchema.methods.generateHash = function (password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
-}
-
-userSchema.methods.validPassword = function (password) {
-  return bcrypt.compareSync(password, this.local.password)
-}
-export {
-  userSchema
-}
