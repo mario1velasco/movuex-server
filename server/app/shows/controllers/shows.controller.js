428602123dade3ab8194a316d73ee97325f4c842
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:48:11 +0200

    create show controller

--- server/app/shows/controllers/shows.controller.js
+++ server/app/shows/controllers/shows.controller.js
@@ -38,4 +38,26 @@ export class ShowsController extends BaseController {
       this.checkForError(res, err)
     }
   }
+
+  addVote (req, res) {
+    try {
+      const showId = req.params.showId
+      this.showBusiness.addVote(showId)
+        .then(response => res.send(response))
+        .catch(this.checkForError.bind(res))
+    } catch (err) {
+      this.checkForError(res, err)
+    }
+  }
+
+  addNote (req, res) {
+    try {
+      const showId = req.params.showId
+      this.showBusiness.addNote(showId, req.body)
+        .then(response => res.send(response))
+        .catch(this.checkForError.bind(res))
+    } catch (err) {
+      this.checkForError(res, err)
+    }
+  }
 }
