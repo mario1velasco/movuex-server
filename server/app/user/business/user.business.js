8663652f933c65dc7b5a92711f7d46e43c9b8d2f
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:50:46 +0200

    user business logic¡

--- server/app/user/business/user.business.js
+++ server/app/user/business/user.business.js
@@ -6,7 +6,39 @@ export class UserBusiness {
   }
 
   logged (user) {
-    const vote = Object.assign(new User(), user)
-    return vote.save()
+    return this.repository.findOne({ 'twitter.id': user.twitter.id })
+      .then(response => {
+        if (!response) {
+          return this.save(user)
+        }
+        return response
+      })
+  }
+
+  save (user) {
+    return new Promise((resolve, reject) => {
+      const newUser = Object.assign(new User(), user)
+      newUser.save((err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve(newUser)
+      })
+    })
+  }
+
+  findUser (query) {
+    return this.repository.findOne(query)
+  }
+
+  createUserFromTwitterProfile (profile, token) {
+    return this.save({
+      twitter: {
+        id: profile.id,
+        token: token,
+        username: profile.username,
+        displayName: profile.displayName
+      }
+    })
   }
 }
