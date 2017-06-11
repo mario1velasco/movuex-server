288237fca8dcef6ce75932f76e8e3adf1534f2a7
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:50:20 +0200

    server main file

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 import http from 'http'
+import path from 'path'
 import express from 'express'
-import { Socket } from './config/components/socket/socket'
+import { default as socket } from './config/components/socket/socket'
 import { BaseMiddlewares } from './config/middlewares/base.middlewares'
 import { connectWithMongoDB } from './config/infrastructure/persistence/mongo/mongoose'
 import { BaseRoutes } from './routes/base.routes'
 import passport from 'passport'
 import { Passport } from './config/components/passport/passport'
 import session from 'express-session'
+const port = process.env.PORT || 3000
+
 Passport.configure(passport)
 connectWithMongoDB()
 
 const app = express()
+const distDir = path.resolve(__dirname, '../../movuex-bulma/dist')
+app.use(express.static(distDir))
+app.set('port', port)
+
 const server = http.createServer(app)
-const socket = Socket.getInstance(server)
-socket.on('socket-opened', (socket) => {
-  console.log(`Connected socket with id: ${socket.id}`)
-})
+socket(server)
 
 app.use(session({
   secret: 'ilovenodejs',
@@ -28,5 +32,4 @@ app.use(passport.session())
 BaseMiddlewares.config(app)
 BaseRoutes.createRoutes(app, passport)
 
-const port = process.env.PORT || 3000
 server.listen(port, () => console.log(`server listen to port ${port}`))
