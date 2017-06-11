5e87f409c1198eb214d89435e0a793c9bd5eec42
Andrea Zucchini <azga84@gmail.com>
2017-06-11 17:43:10 +0200

    deployment scripts

---
+++ bash/deployment/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm pack ../
+scp movuex-0.1.0.tgz root@$1:~
+scp -r config root@$1:~
+scp provision.sh root@$1:~
+ssh root@$1 "./provision.sh"
---
+++ bash/deployment/provision.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+## Install basic development tools and nginx
+apt-get update
+apt-get install -y build-essential git nginx libkrb5-dev
+
+## Install Databases
+apt-get install -y mongodb redis-server
+
+## Install Node.js 6x from NodeSource Distributions
+curl -sL https://deb.nodesource.com/setup_6.x | -E bash -
+apt-get install -y nodejs
+
+## Copy configuration to real destinations
+cp /root/config/default /etc/nginx/sites-enabled/default
+cp /root/config/movuex-1.conf /etc/init
+cp /root/config/movuex-2.conf /etc/init
+cp /root/config/movuex-3.conf /etc/init
+
+## Install movuex
+rm -rf /opt/movuex
+mkdir -p /opt/movuex
+tar xvfz /root/movuex-0.1.0.tgz -C /opt/movuex
+cd /opt/movuex/package && npm install
+
+## Run services
+service nginx restart
+service movuex-1 restart
+service movuex-2 restart
+service movuex-3 restart
