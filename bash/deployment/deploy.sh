1be2f043666cfcc52fecc337ae56c9dd77ae94ad
Andrea Zucchini <azga84@gmail.com>
2017-07-15 23:48:31 +0200

    change package name

--- bash/deployment/deploy.sh
+++ bash/deployment/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm pack ../../
-scp movuex-0.1.0.tgz root@$1:~
+scp movuex-server-0.1.0.tgz root@$1:~
 scp -r config root@$1:~
 scp provision.sh root@$1:~
 ssh root@$1 "./provision.sh"
