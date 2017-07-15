362cd17e61c7efc22f4f718a9296d36907dfe926
Andrea Zucchini <azga84@gmail.com>
2017-07-16 01:42:46 +0200

    check if stack property exist

--- server/app/shows/controllers/base/base.controller.js
+++ server/app/shows/controllers/base/base.controller.js
@@ -2,7 +2,17 @@ import logger from '../../../../config/infrastructure/persistence/logger/logger'
 
 export class BaseController {
   checkForError (res, err) {
-    logger.error(err.stack)
+    let msg = ''
+    try {
+      if (typeof err.stack !== 'undefined') {
+        msg = err.stack
+      } else {
+        msg = JSON.stringify(err)
+      }
+      logger.error(msg)
+    } catch (e) {
+      logger.error('Base controller error')
+    }
     res.send({error: 'error in your request'})
   }
 }
